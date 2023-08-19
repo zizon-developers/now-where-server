fd86cd58dddfede6ebef944a4c1bac1e2e17bfb9
uhanuu <hwryu990927@gmail.com>
2023-08-20 02:30:26 +0900

    refactor: environment, resttemplatet이용

--- user-service/src/main/java/com/spring/userservice/auth/OAuthKakaoService.java
+++ user-service/src/main/java/com/spring/userservice/auth/OAuthKakaoService.java
@@ -4,15 +4,14 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.spring.userservice.auth.exception.KakaoFriendsException;
 import com.spring.userservice.service.UserService;
-import com.spring.userservice.vo.ResponseOrder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -21,7 +20,6 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
@@ -29,98 +27,75 @@ import java.util.*;
 @Slf4j
 @RequiredArgsConstructor
 public class OAuthKakaoService {
-    private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = new RestTemplate(){};
     private final Environment evn;
     private final UserService userService;
 
     public void getKakaoAccessToken (String code) {
-        String access_Token = "";
-        String refresh_Token = "";
-        String reqURL = "https://kauth.kakao.com/oauth/token";
+        ResponseEntity<Map> response = null;
+        String accessToken = "";
+        String refreshToken = "";
+        String reqURL = evn.getProperty("spring.security.oauth2.client.provider.kakao.token-uri");
 
         try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
 
-            //POST 요청을 위해 기본값이 false인 setDoOutput을 true로
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-//            conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset=utf-8");
+            MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
+            body.add("grant_type", evn.getProperty("spring.security.oauth2.client.registration.kakao.authorization-grant-type"));
+            body.add("client_id", evn.getProperty("spring.security.oauth2.client.registration.kakao.client-id"));
+            body.add("client_secret", evn.getProperty("spring.security.oauth2.client.registration.kakao.client-secret"));
+            body.add("redirect_uri", evn.getProperty("spring.security.oauth2.client.registration.kakao.redirect-uri"));
+            body.add("code", code);
 
-            //POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=authorization_code");
-            sb.append("&client_id=867de5dcb061c1d22c4188893654cddb"); // TODO REST_API_KEY 입력
-            sb.append("&client_secret=bXXA2RVw5KmpoVjrHns8xGvOHZXvSizK"); // TODO REST_API_KEY 입력
-            sb.append("&redirect_uri=http://localhost:8000/user-service/oauth/callback/kakao"); // TODO 인가코드 받은 redirect_uri 입력
-            sb.append("&code=" + code);
-            bw.write(sb.toString());
-            bw.flush();
+            HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(body, headers);
 
-            //결과 코드가 200이라면 성공
-            int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
 
-            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
+            response = restTemplate.exchange(reqURL, HttpMethod.POST, entity, Map.class);
+            log.info("status code ={}",response.getStatusCode());
+            log.info("bode ={}",response.getBody());
 
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-            System.out.println("response body : " + result);
+            if (response.getStatusCode() != HttpStatus.OK)
+                throw new KakaoFriendsException("Failed to retrieve Kakao friends. Status code: " + response.getStatusCode());
 
-            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-//            JsonParser parser = new JsonParser();
-//            JsonElement element = parser.parse(result);
-            JsonElement element = JsonParser.parseString(result);
+            Map responseBody = response.getBody();
+            accessToken = (String) responseBody.get("access_token");
+            refreshToken = (String) responseBody.get("refresh_token");
+            log.info("access ={}",accessToken);
+            log.info("refresh ={}",refreshToken);
 
-            access_Token = element.getAsJsonObject().get("access_token").getAsString();
-            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
 
-            log.info("access_token ={}",access_Token);
-            log.info("refresh_toke ={}",refresh_Token);
-
-            br.close();
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            log.error("error",e);
+            throw new KakaoFriendsException("Failed to retrieve Kakao friends. Status code: " + response.getStatusCode());
         }
-
-        createKakaoUser(access_Token);
+        createKakaoUser(accessToken);
     }
 
-    private void createKakaoUser(String token) {
+    private void createKakaoUser(String accessToken) {
 
-        String reqURL = "https://kapi.kakao.com/v2/user/me";
+        String reqURL = evn.getProperty("spring.security.oauth2.client.provider.kakao.user-info-uri");
 
         //access_token을 이용하여 사용자 정보 조회
         try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token전송
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
+            HttpEntity<String> entity = new HttpEntity<>(headers);
 
-            //결과 코드가 200이라면 성공
-            int responseCode = conn.getResponseCode();
-            log.info("responseCode ={}",responseCode);
+            ResponseEntity<String> response = restTemplate.exchange(reqURL, HttpMethod.POST, entity, String.class);
 
-            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
+            log.info("status ={}",response.getStatusCode());
+            if (response.getStatusCode() != HttpStatus.OK)
+                throw new KakaoFriendsException("Failed to retrieve Kakao friends. Status code: " + response.getStatusCode());
 
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-            log.info("response body ={} ",result);
+            String responseBody = response.getBody();
+            log.info("body ={}",responseBody);
+
 
             //Gson 라이브러리로 JSON파싱
-            JsonElement element = JsonParser.parseString(result);
+            JsonElement element = JsonParser.parseString(responseBody);
 
             int id = element.getAsJsonObject().get("id").getAsInt();
             boolean hasEmail = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_email").getAsBoolean();
@@ -129,12 +104,10 @@ public class OAuthKakaoService {
                 email = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
             }
 
-            log.info("id ={}" + id);
-            log.info("email ={}" + email);
+            log.info("id ={}" ,id);
+            log.info("email ={}" ,email);
 
-            br.close();
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -145,7 +118,8 @@ public class OAuthKakaoService {
         headers.set(HttpHeaders.AUTHORIZATION, accessToken);
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        String reqURL = "https://kapi.kakao.com/v1/api/talk/friends";
+
+        String reqURL = evn.getProperty("spring.security.oauth2.client.provider.kakao.friend-uri");
         URI targetUrl = UriComponentsBuilder
                 .fromUriString(reqURL) // 기본 URL
                 .queryParam("limit", 15)
